8d0b8f0c05d330bc29943877774af5752f03e54f
topherh <cheiland@uw.edu>
2009-03-05 20:34:25 +0000

    this is totally nuts

--- Slim_Header/blanktextbox.js
+++
@@ -1,4 +0,0 @@
-function make_blank()
-{
-document.formA.q.value ="";
-}
--- UW-main_files/home2006pre.js
+++
@@ -1,6 +0,0 @@
-document.cssonly = 1;
-document.flyout_disable = window.location.href.indexOf ('?nojavascript') > 0 ||
-							! document.childNodes ||
-							(navigator.userAgent.indexOf ('MSIE') > 0 &&
-							navigator.userAgent.indexOf ('Mac') > 0 &&
-							navigator.userAgent.indexOf ('Opera') < 0);
