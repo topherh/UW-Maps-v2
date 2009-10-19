865f95269848ee3e1682ad05677c77674813fc55
topherh <cheiland@uw.edu>
2009-10-19 18:26:43 +0000

    Copy and Paste now works for Windows, possibly others, fails on linux.  part 2

--- scripts/functions.js
+++ scripts/functions.js
@@ -1,3 +1,26 @@
+function copy_to_clipboard(text)
+{
+    if(window.clipboardData)
+    {
+        window.clipboardData.setData('text',text);
+    }
+    else
+    {
+        var clipboarddiv=document.getElementById('divclipboardswf');
+        if(clipboarddiv==null)
+        {
+           clipboarddiv=document.createElement('div');
+           clipboarddiv.setAttribute("name", "divclipboardswf");
+           clipboarddiv.setAttribute("id", "divclipboardswf");
+           document.body.appendChild(clipboarddiv);
+        }
+        clipboarddiv.innerHTML='<embed src="/maps/clipboard.swf" FlashVars="clipboard='+
+            encodeURIComponent(text)+'" width="0" height="0" type="application/x-shockwave-flash"></embed>';
+    }
+    alert('The location is copied to your clipboard...');
+    return false;
+}
+
 // extinfowindow.js:::
 /*
 * ExtInfoWindow Class, v1.0 
